bc488fa9968618232be57a40c6fb673bd940916a
atanweer <tanweer@uw.edu>
2017-01-15 16:32:53 -0800

    Commented out notes I took while going over solutions during class

--- week_02_programming_challenges_anissa.R
+++ week_02_programming_challenges_anissa.R
@@ -31,6 +31,8 @@ midpoint<-length(week2.dataset.sorted)/2
 midpoint
 #call the value that exists at the midpoint of the dataset
 week2.dataset.sorted[midpoint]
+#Note: This isn't actually the true median because there is an even number of
+#entries in the dataset, so I should have averaged the 50th and 51st observations
 
 #MODE
 #sort dataset
@@ -39,6 +41,22 @@ week2.dataset.sorted<-sort(week2.dataset)
 table(week2.dataset.sorted)
 #all values appear only once, so we can do a histogram to see what segmented modes appear
 hist(week2.dataset.sorted)
+#Note: How Mako showed us to do this:
+#week2.dataset.round <- round(week2.dataset)
+#tail(sort(table(week2.dataset.round)))
+#OR:
+#max.count <- rev(sort(table(week2.dataset)))[1]
+#could do that with the rounded dataset, but there usually aren't modes with
+#continuous data
+#OR: 
+#table(week2.dataset.round)[table(week2.dataset.round) == max.count]
+#saying give me each value in the table and tell me if each one is equal to max.count
+#that way you get all the modes, not just the top one
+#what the above is doing is creating a true-false vector, making a subset of the data
+#going to be doing this over and over; using a vector to read into itself
+#how to get only some of the lines of a data
+#R's contribution to the world
+
 
 #PC5:
 #BOXPLOT
@@ -47,6 +65,8 @@ boxplot(week2.dataset)
 hist(week2.dataset)
 #DOTCHART
 dotchart(week2.dataset)
+#Note: How to do density plot:
+#plot(density(week2.dataset))
 
 #PC6:
 #find all entries that are negative numbers in dataset
@@ -62,6 +82,9 @@ sort(week2.dataset)
 #generate a dataset that excludes the "NA" values
 week2.dataset.pos <- sort(week2.dataset)
 #print the dataset that excludes the "NA" values
+#NOTE: THIS IS WHAT I WAS SUPPOSED TO DO: mean(week2.dataset, na.rm=TRUE)
+#My sorting hack worked, but "mean(week2.dataset, na.rm=TRUE)" would have removed
+#anything that's rm
 week2.dataset.pos
 #find the mean of the dataset that excludes the "NA" values
 mean(week2.dataset.pos)
@@ -69,6 +92,7 @@ mean(week2.dataset.pos)
 #find the standard deviation of the dataset that excludes "NA" values
 sd(week2.dataset.pos)
 #this sd (38.22241) is smaller than the sd that included negative values (45.6457)
+
 
 #PC7:
 #log transform dataset (the new one that excludes negative numbers) and create new variable
